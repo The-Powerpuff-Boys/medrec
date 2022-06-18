bc4aac85b4acb67560ba436392b29aa9b3ab876c
harshsH-28 <harshsharma6419@gmail.com>
2022-06-18 14:56:10 +0530

    Initialized Backend

---
+++ medrec-backend/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+require("dotenv").config();
+
+const Doctor = require("./models/doctor");
+
+mongoose
+  .connect("mongodb://localhost:27017/med-rec", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database Error");
+    console.log(err);
+  });
+
+const port = process.env.PORT || 8080;
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24,
+    maxAge: Date.now() + 1000 * 60 * 60 * 24,
+  },
+};
+
+app.use(session(sessionConfig));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(Doctor.authenticate()));
+
+passport.serializeUser(Doctor.serializeUser());
+passport.deserializeUser(Doctor.deserializeUser());
+
+app.use("/", require("./routes/api"));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
