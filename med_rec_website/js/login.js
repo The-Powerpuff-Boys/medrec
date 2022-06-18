bfcdb7e18c7f92d56ff0bcb40b141faebd5d473d
Kranium2002 <vidhumathur2002@gmail.com>
2022-06-18 16:18:36 +0530

    Signup
    Sign Up fields added

--- med_rec_website/js/login.js
+++ med_rec_website/js/login.js
@@ -3,18 +3,18 @@ function EnableDisable(InputEmail1) {
     var btnSubmit = document.getElementById("submit");
     var em = document.getElementById("InputEmail1");
     var pass = document.getElementById("InputPassword1");
-    var flag = 0;
+
     //Verify the TextBox value.
 
     if (em.value == "" || pass.value == "") {
-
+        //Disable button
         btnSubmit.disabled = true;
         btnSubmit.className = "btn btn-secondary";
 
 
 
     } else {
-        //Disable the TextBox when TextBox is empty.
+        //Enable button
         btnSubmit.disabled = false;
         btnSubmit.className = "btn btn-primary";
     }
