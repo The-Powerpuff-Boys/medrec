340ab04ce0c2ce1215fb4df92983d749f2db9042
2002Bishwajeet <bishwajeet.techmaster@gmail.com>
2022-06-19 06:20:47 +0530

    fix model

--- medrec_app/lib/models/patient.dart
+++ medrec_app/lib/models/patient.dart
@@ -4,14 +4,16 @@ import '../utils/gender.dart';
 
 class Patient {
   final String fullName;
-  final String age;
+  final int age;
   final Gender? gender;
+  final String imgUrl;
   final int abha;
   final Diseases? diseases;
   Patient({
     required this.fullName,
     required this.age,
     this.gender,
+    required this.imgUrl,
     required this.abha,
     this.diseases,
   });
