fe7c367f94edaf5b88d08e189ebaa75d86be8afc
Kranium2002 <vidhumathur2002@gmail.com>
2022-06-23 01:13:19 +0530

    Final_UI_Push

--- medrec-backend/public/js/signup.js
+++ medrec-backend/public/js/signup.js
@@ -1,13 +1,19 @@
 function EnableDisable(InputEmail1) {
     //Reference the Button.
     var btnSubmit = document.getElementById("submit");
-    var em = document.getElementById("InputEmail1");
-    var pass = document.getElementById("InputPassword1");
-    var pass1 = document.getElementById("InputPassword2");
-    var flag = 0;
+    var _1 = document.getElementById("fullname");
+    var _2 = document.getElementById("Qual");
+    var _3 = document.getElementById("gender");
+    var _4 = document.getElementById("phno");
+    var _5 = document.getElementById("InputEmail1");
+    var _6 = document.getElementById("InputUsername1");
+    var pass1 = document.getElementById("InputPassword1");
+    var pass2 = document.getElementById("InputPassword2");
+
+
     //Verify the TextBox value.
 
-    if (em.value == "" || pass.value == "" || pass1.value == "") {
+    if (_1.value == "" || _2.value == "" || _3.value == "" || _4.value == "" || _5.value == "" || _6.value == "" || pass1.value == "" || pass2.value == "") {
 
 
 
@@ -22,7 +28,7 @@ function EnableDisable(InputEmail1) {
         btnSubmit.disabled = false;
         btnSubmit.className = "btn btn-primary";
     }
-    if (pass1.value != pass.value) {
+    if (pass1.value != pass2.value) {
         document.getElementById("alert").style.display = "block";
     } else {
         document.getElementById("alert").style.display = "none";
