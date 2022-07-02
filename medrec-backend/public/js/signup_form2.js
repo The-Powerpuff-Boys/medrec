c435f153cb7bf2902c2b9e300d5146f73c13cb5c
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2022-07-03 00:11:55 +0530

    Delete medrec-backend directory

--- medrec-backend/public/js/signup_form2.js
+++
@@ -1,8 +0,0 @@
-function dropdown(val) {
-  document.getElementById("dp_button").innerHTML = val;
-}
-
-function myfunction() {
-  let abha = document.getElementById("sb").value;
-  location.replace(`/patientinfo/${abha}`);
-}
